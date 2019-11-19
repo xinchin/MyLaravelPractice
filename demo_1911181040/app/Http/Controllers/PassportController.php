22973ee1b173df9f669af9d7dab946031b5e7eb7
Nelson Huang <xinchin@gmail.com>
2019-11-19 08:49:12 +0800

    Passport 認證：註冊

--- demo_1911181040/app/Http/Controllers/PassportController.php
+++ demo_1911181040/app/Http/Controllers/PassportController.php
@@ -2,9 +2,67 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Validator;
 
 class PassportController extends Controller
 {
-    //
+    protected $clientId;
+    protected $clientSecret;
+
+    public function __construct(){
+        $this->middleware('auth')->except('login', 'register', 'refresh');
+        // $client = DB::table('oauth_clients')->where('id', 2)->first();
+        $client = Cache::remember('password_client', 60*5, function(){
+            return DB::table('oauth_clients')->where('id', 2)->first();
+        });
+        $this->clientId = $client->id;
+        $this->clientSecret = $client->secret;
+    }
+
+    protected function username(){
+        return 'email';
+    }
+
+    protected function validator(array $data){
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:8', 'confirmed']
+        ]);
+    }
+
+    protected function create(array $data){
+        return User::forceCreate([
+            'name'=>$data['name'],
+            'email'=>$data['email'],
+            'password'=>\password_hash($data['password'], PASSWORD_DEFAULT),
+        ]);
+    }
+
+    private function getToken(){
+        $response = (new Client())->post('http://localhost:8000/api/oauth/token',[
+            'form_params' =>[
+                'grant_type'=>'password',
+                'username'=>request('email'),
+                'password'=>request('password'),
+                'client_id'=>$this->clientId,
+                'client_secret'=>$this->clientSecret,
+                'scope'=>'*',
+            ]
+        ]);
+        return $response->getBody();
+    }
+
+    public function register(){
+        $this->validator(request()->all())->validate();
+        $this->create(request()->all());
+        // return request()->all();
+        return $this->getToken();
+    }
+
 }
